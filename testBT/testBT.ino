9e42b430c25c55695c8ba1d3d9cb90a64dd4efa6
gregiorg <gergely.fodor@etu.univ-grenoble-alpes.fr>
2023-03-15 14:12:16 +0100

    tesing bluetooth connection

---
+++ testBT/testBT.ino
@@ -0,0 +1,30 @@
+#include "BluetoothSerial.h"
+
+#include <Wire.h> 
+
+#include <SparkFun_u-blox_GNSS_v3.h> //http://librarymanager/All#SparkFun_u-blox_GNSS_v3
+SFE_UBLOX_GNSS myGNSS;
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+
+  Wire.begin();
+
+  if (myGNSS.begin() == false) //Connect to the u-blox module using Wire port
+  {
+    Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
+
+  myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
